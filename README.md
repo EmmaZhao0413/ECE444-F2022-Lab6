fe8c898c1328047d12f9983f111bb4e763120922
EmmaZhao0413 <emmazhao@LAPTOP-HDJSDEGP.localdomain>
2022-11-13 09:41:11 -0500

    first test

--- README.md
+++
@@ -1,3 +0,0 @@
-# ECE444-F2022-Lab6
-
-This repo is a copy from https://github.com/mjhea0/flaskr-tdd
---
+++ project/app.py
@@ -0,0 +1,15 @@
+# app project
+from flask import Flask
+
+
+# create and initialize a new Flask app
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+
+if __name__ == "__main__":
+    app.run()
