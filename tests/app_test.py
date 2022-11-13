cdef2146d59aee81d28d464bf981b4098d8435d2
EmmaZhao0413 <emmazhao@LAPTOP-HDJSDEGP.localdomain>
2022-11-13 09:48:55 -0500

    database setup

--- tests/app_test.py
+++ tests/app_test.py
@@ -1,6 +1,7 @@
 # test app functions
-from project.app import app
+from pathlib import Path
 
+from project.app import app
 
 def test_index():
     tester = app.test_client()
@@ -8,3 +9,8 @@ def test_index():
 
     assert response.status_code == 200
     assert response.data == b"Hello, World!"
+
+
+def test_database():
+    init_db()
+    assert Path("flaskr.db").is_file()
